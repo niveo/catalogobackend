50c68d112052e4890a1b200f1fe73d40f0d03cc0
amsraimar <amsraimar@gmail.com>
2023-11-27 17:13:25 -0300

    Teste Inicial mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://sandnine:y4nWwChddMP7NtpW@cluster0.f4qpzni.mongodb.net/catalogodb',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@InjectConnection() private connection: Connection) {}
+  getHello(): any {
+    return this.connection.collection('catalogo').find().toArray();
   }
 }
