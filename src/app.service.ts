069a3b6509ae49b8e4813b96c41da03575f1d885
amsraimar <amsraimar@gmail.com>
2023-12-20 11:38:23 -0300

    Variaveis para profile

--- src/app.service.ts
+++ src/app.service.ts
@@ -34,6 +34,21 @@ export class AppService {
           'X'.padEnd(this.config.get('PGPASSWORD').length, 'X'),
         ),
       database: this.config.get('PGDATABASE'),
+      VERCEL_ENV: this.config.get('VERCEL_ENV'),
+      VERCEL_BRANCH_URL: this.config.get('VERCEL_BRANCH_URL'),
+      VERCEL_GIT_PROVIDER: this.config.get('VERCEL_GIT_PROVIDER'),
+      VERCEL_GIT_REPO_ID: this.config.get('VERCEL_GIT_REPO_ID'),
+      VERCEL_GIT_COMMIT_REF: this.config.get('VERCEL_GIT_COMMIT_REF'),
+      VERCEL_GIT_COMMIT_SHA: this.config.get('VERCEL_GIT_COMMIT_SHA'),
+      VERCEL_GIT_COMMIT_MESSAGE: this.config.get('VERCEL_GIT_COMMIT_MESSAGE'),
+      VERCEL_GIT_COMMIT_AUTHOR_LOGIN: this.config.get(
+        'VERCEL_GIT_COMMIT_AUTHOR_LOGIN',
+      ),
+      VERCEL_GIT_COMMIT_AUTHOR_NAME: this.config.get(
+        'VERCEL_GIT_COMMIT_AUTHOR_NAME',
+      ),
+      VERCEL_GIT_PREVIOUS_SHA: this.config.get('VERCEL_GIT_PREVIOUS_SHA'),
+      VERCEL_GIT_PULL_REQUEST_ID: this.config.get('VERCEL_GIT_PULL_REQUEST_ID'),
     };
   }
 
